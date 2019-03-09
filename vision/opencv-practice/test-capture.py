2c728796ef3c95d469f7c57780b00ff29c7207f6
explorer-ading <explorer.ading@yahoo.com>
2019-03-09 22:24:21 +0800

    cv practice - image processing.

---
+++ vision/opencv-practice/test-capture.py
@@ -0,0 +1,17 @@
+
+
+import cv2
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+rval, frame = vc.read()
+
+while True:
+
+  if frame is not None:
+     cv2.imshow("preview", frame)
+  rval, frame = vc.read()
+
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+     break
+
